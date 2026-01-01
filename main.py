cb41fc6aa1f8119bb12dd015c50078a636863636
omkar3311 <omkarwaghmare3311@gmail.com>
2026-01-01 19:20:22 +0530

    YOLO added

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import cv2
-from fastapi import FastAPI, Request
+from fastapi import FastAPI,Request
 from fastapi.responses import StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from ultralytics import YOLO
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+model = YOLO("yolov8n.pt")
 
 camera = cv2.VideoCapture(0)
 
@@ -15,24 +15,31 @@ def generate_frames():
     while True:
         success, frame = camera.read()
         if not success:
-            break
+            continue
 
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame_bytes = buffer.tobytes()
+        frame = cv2.flip(frame,1)
+        results = model(frame, conf=0.4, classes=[0])
+        for r in results:
+            for box in r.boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                cv2.rectangle(frame, (x1, y1), (x2, y2),
+                              (0, 255, 0), 2)
+                cv2.putText(frame, "Person",
+                            (x1, y1 - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7, (0, 255, 0), 2)
 
+        _, buffer = cv2.imencode(".jpg", frame)
         yield (
-            b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' +
-            frame_bytes +
-            b'\r\n'
+            b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" +
+            buffer.tobytes() +
+            b"\r\n"
         )
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
-
+    return templates.TemplateResponse("index.html", {"request": request})
+    
 @app.get("/video_feed")
 def video_feed():
     return StreamingResponse(
